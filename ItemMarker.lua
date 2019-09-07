d2324c1717f31be1273bd89c5c86b0eeb9e32daa
Javier Rodríguez Posada <javiropos@gmail.com>
2019-09-07 21:13:13 +0200

    Remove commented code. Extract info from control on method. Fix ZO_... calls

--- ItemMarker.lua
+++ ItemMarker.lua
@@ -3,27 +3,9 @@ ItemMarker = {}
 ItemMarker.name = "ItemMarker"
 ItemMarker.event = "ItemMarkerEvent"
 ItemMarker.savedVariablesFile = "ItemMarkerSavedVariables"
-ItemMarker.slotsLists = {
-    BACKPACK = ZO_PlayerInventoryList,
-    CRAFTBAG = ZO_CraftBagList
-}
-
 ItemMarker.inventories = {
     1
 }
-
--- local LISTS = {
---     BACKPACK = ZO_PlayerInventoryList,
---     QUICKSLOT = ZO_QuickSlotList,
---     BANK = ZO_PlayerBankBackpack,
---     GUILD_BANK = ZO_GuildBankBackpack,
---     CRAFTBAG = ZO_CraftBagList,
---     DECONSTRUCTION = ZO_SmithingTopLevelDeconstructionPanelInventoryBackpack,
---     IMPROVEMENT = ZO_SmithingTopLevelImprovementPanelInventoryBackpack,
---     ENCHANTING = ZO_EnchantingTopLevelInventoryBackpack,
---     ALCHEMY = ZO_AlchemyTopLevelInventoryBackpack,
---     LIST_DIALOG = ZO_ListDialog1List,
--- }
 
 function ItemMarker:ToogleItem(itemInstanceId, inventorySlot)
     ItemMarkerData:ToogleItem(itemInstanceId)
@@ -31,26 +13,9 @@ function ItemMarker:ToogleItem(itemInstanceId, inventorySlot)
 end
 
 function ItemMarker:AddMenuOption(control)
-    local bag, index
-    local link = nil
-    local slotType = ZO_control_GetType(control)
-    
-    if slotType == SLOT_TYPE_ITEM or
-       slotType == SLOT_TYPE_EQUIPMENT or
-       slotType == SLOT_TYPE_STORE_BUY or
-       slotType == SLOT_TYPE_STORE_BUYBACK or
-       slotType == SLOT_TYPE_BANK_ITEM or
-       slotType == SLOT_TYPE_GUILD_BANK_ITEM or
-       slotType == SLOT_TYPE_LOOT or
-       slotType == SLOT_TYPE_CRAFTING_COMPONENT or
-       slotType == SLOT_TYPE_PENDING_CRAFTING_COMPONENT or
-       slotType == SLOT_TYPE_CRAFT_BAG_ITEM then
-        bag, index = ZO_Inventory_GetBagAndIndex(control)
-        link = GetItemLink(bag, index)
-    end
+    local itemInstanceId = ItemMarkerControl:GetInfoFromControl(control)
 
-    if link and ItemMarkerUtils:IsItemLink(link) then
-        local itemInstanceId = GetItemInstanceId(bag, index)
+    if itemInstanceId then
         local isItemMarked = ItemMarkerData:IsItemMarked(itemInstanceId)
 
         if not isItemMarked then
@@ -65,69 +30,25 @@ function ItemMarker:AddMenuOption(control)
     end
 end
 
--- function ItemMarker:HookBags()
---     -- PLAYER_INVENTORY (backpack/quest_item/bank/house_bank/guild_bank/craft_bag)
---     for k,v in pairs(PLAYER_INVENTORY.inventories) do
---       local listView = v.listView
---       if ( listView and listView.dataTypes and listView.dataTypes[1] ) then
---         ZO_PreHook(listView.dataTypes[1], "setupCallback", function(control, slot)
---           local itemLink = GetItemLink(control.dataEntry.data.bagId, control.dataEntry.data.slotIndex, LINK_STYLE_BRACKETS)
---           self:SetInventoryIcon(control, itemLink)
---         end)
---       end
---     end
--- end
-
-
--- local hookedSetupFunctions = {}
---     local function newSetupCallback(rowControl, slot)
---         local listViewName = rowControl:GetParent():GetParent():GetName()
-
---         if hookedSetupFunctions[listViewName] then
---             hookedSetupFunctions[listViewName](rowControl, slot)
---         end
-
---         util.CreateMarkerControl(rowControl)
---     end
-
---     --list hooks
---     for _, list in pairs(LISTS) do
---         hookedSetupFunctions[list:GetName()] = list.dataTypes[1].setupCallback
---         list.dataTypes[1].setupCallback = newSetupCallback
---     end
-
 function ItemMarker:HookControls()
-    local slotType = ZO_control_GetType(control)
-    if slotType == SLOT_TYPE_ITEM or
-       slotType == SLOT_TYPE_EQUIPMENT or
-       slotType == SLOT_TYPE_STORE_BUY or
-       slotType == SLOT_TYPE_STORE_BUYBACK or
-       slotType == SLOT_TYPE_BANK_ITEM or
-       slotType == SLOT_TYPE_GUILD_BANK_ITEM or
-       slotType == SLOT_TYPE_LOOT or
-       slotType == SLOT_TYPE_CRAFTING_COMPONENT or
-       slotType == SLOT_TYPE_PENDING_CRAFTING_COMPONENT or
-       slotType == SLOT_TYPE_CRAFT_BAG_ITEM then
-        bag, index = ZO_Inventory_GetBagAndIndex(control)
-        link = GetItemLink(bag, index)
-    end
-
     for inventoryType, inventory in pairs(PLAYER_INVENTORY.inventories) do
         if self.inventories[inventoryType] and
            inventory.listView and inventory.listView.dataTypes and inventory.listView.dataTypes[1] then
-            ZO_PreHook(listView.dataTypes[1], "setupCallback", function(control, slot)
-                local itemInstanceId = GetInstanceId(control.dataEntry.data.bagId, control.dataEntry.data.slotIndex)
-                self:RefreshControl(itemInstanceId, control)
-            end)
+            -- ZO_PreHook(listView.dataTypes[1], "setupCallback", function(control, slot)
+            --     d(inventoryType)
+            --     local itemInstanceId = ItemMarkerControl:GetInfoFromControl(control)
+            --     if itemInstanceId then
+            --         ItemMarkerControl:RefreshControl(itemInstanceId, control)
+            --     end
+            -- end)
         end
-        -- https://www.esoui.com/forums/archive/index.php/t-2182.html
     end
 end
 
 function ItemMarker:Initialize()
     ItemMarkerData:Initialize()
-    -- self:HookControls()
-    ZO_PreHook("ZO_control_ShowContextMenu", function(control) self:AddMenuOption(control) end)
+    self:HookControls()
+    ZO_PreHook("ZO_InventorySlot_ShowContextMenu", function(control) self:AddMenuOption(control) end)
 end
 
 local function OnAddOnLoaded(eventCode, addonName)
--- ItemMarkerControl.lua
+++ ItemMarkerControl.lua
@@ -2,7 +2,7 @@ ItemMarkerControl = {}
 
 ItemMarkerControl.markerName = "ItemMarker"
 
-function ItemMarkerControl:RefreshControl(itemInstanceId, inventorySlot)
+function ItemMarkerControl:RefreshControl(itemInstanceId, control)
     local function getMarkerAnchorOffsets(markerAnchor)
         local offsetValue = 10
         local offsets = {
@@ -48,24 +48,24 @@ function ItemMarkerControl:RefreshControl(itemInstanceId, inventorySlot)
     end
 
     local isItemMarked = ItemMarkerData:IsItemMarked(itemInstanceId)
-    local control = inventorySlot:GetNamedChild(self.markerName)
+    local markerControl = control:GetNamedChild(self.markerName)
 
-    if control and not isItemMarked then
-        control:SetHidden(true)
+    if markerControl and not isItemMarked then
+        markerControl:SetHidden(true)
         return
     elseif not isItemMarked then
         return
     end
 
-    if not control then
-        control = WINDOW_MANAGER:CreateControl(inventorySlot:GetName() .. ItemMarkerControl.markerName, inventorySlot, CT_TEXTURE)
-        control:SetDimensions(32, 32)
-        control:SetDrawTier(DT_HIGH)
+    if not markerControl then
+        markerControl = WINDOW_MANAGER:CreateControl(control:GetName() .. ItemMarkerControl.markerName, control, CT_TEXTURE)
+        markerControl:SetDimensions(32, 32)
+        markerControl:SetDrawTier(DT_HIGH)
     end
 
     local texturePath, r, g, b = ItemMarkerData:GetMarkerInfo(itemInstanceId)
     local markerAnchor = ItemMarkerData:GetMarkerAnchor()
-    local anchorTarget = inventorySlot:GetNamedChild("Button")
+    local anchorTarget = control:GetNamedChild("Button")
     local offsetX, offsetY = getMarkerAnchorOffsets(markerAnchor)
 
     if anchorTarget then
@@ -73,15 +73,41 @@ function ItemMarkerControl:RefreshControl(itemInstanceId, inventorySlot)
         anchorTarget = anchorTarget:GetNamedChild("Icon")
     else
         --equipment control
-        anchorTarget = inventorySlot:GetNamedChild("Icon")
+        anchorTarget = control:GetNamedChild("Icon")
     end
 
     --there's no anchor target. How'd we get here?
     if not anchorTarget then return end
 
-    control:SetHidden(false)
-    control:SetTexture(texturePath)
-    control:SetColor(r, g, b)
-    control:ClearAnchors()
-    control:SetAnchor(markerAnchor, anchorTarget, markerAnchor, offsetX, offsetY)    
+    markerControl:SetHidden(false)
+    markerControl:SetTexture(texturePath)
+    markerControl:SetColor(r, g, b)
+    markerControl:ClearAnchors()
+    markerControl:SetAnchor(markerAnchor, anchorTarget, markerAnchor, offsetX, offsetY)    
+end
+
+function ItemMarkerControl:GetInfoFromControl(control)
+    local bag, index
+    local link = nil
+    local slotType = ZO_InventorySlot_GetType(control)
+    
+    if slotType == SLOT_TYPE_ITEM or
+       slotType == SLOT_TYPE_EQUIPMENT or
+       slotType == SLOT_TYPE_STORE_BUY or
+       slotType == SLOT_TYPE_STORE_BUYBACK or
+       slotType == SLOT_TYPE_BANK_ITEM or
+       slotType == SLOT_TYPE_GUILD_BANK_ITEM or
+       slotType == SLOT_TYPE_LOOT or
+       slotType == SLOT_TYPE_CRAFTING_COMPONENT or
+       slotType == SLOT_TYPE_PENDING_CRAFTING_COMPONENT or
+       slotType == SLOT_TYPE_CRAFT_BAG_ITEM then
+        bag, index = ZO_Inventory_GetBagAndIndex(control)
+        link = GetItemLink(bag, index)
+    end
+
+    if link and ItemMarkerUtils:IsItemLink(link) then
+        return GetItemInstanceId(bag, index)
+    end
+
+    return nil
 end
